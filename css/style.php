a369530399f308a83b334e7e6d1cdfae86420268
Ivan Lutrov <lutrov@users.noreply.github.com>
2017-08-07 15:07:18 +1000

    Update style.php

--- css/style.php
+++ css/style.php
@@ -2,23 +2,40 @@
 
 //////////////////////////////////////////////////////////////////////////////
 //
-//	Script: style.php
+//      Script: style.php
 // Description: Dermatos stylesheet controller.
-//	Author: Ivan Lutrov <ivan@lutrov.com>
+//      Author: Ivan Lutrov <ivan@lutrov.com>
 //
 //////////////////////////////////////////////////////////////////////////////
 
 //
-// Include constants used by this script.
+// Define constants used by this script.
 //
+define('DERMATOS_COLOUR_HIGHLIGHT_PRIMARY', '#d71900');
+define('DERMATOS_COLOUR_HIGHLIGHT_SECONDARY', '#0078d7');
 define('DERMATOS_FONT_FAMILY', 'Roboto Condensed');
 define('DERMATOS_FONT_FAMILY_ESCAPED', 'Roboto+Condensed');
 define('DERMATOS_FONT_WEIGHT_NORMAL', '300');
 define('DERMATOS_FONT_WEIGHT_BOLD', '400');
 define('DERMATOS_FONT_WEIGHT_EXTRA_BOLD', '700');
-define('DERMATOS_COLOUR_HIGHLIGHT_PRIMARY', 'f45246');
-define('DERMATOS_COLOUR_HIGHLIGHT_SECONDARY', '18b3dc');
-define('DERMATOS_CACHE_EXPIRY_SECONDS', 3600);
+define('DERMATOS_CACHE_EXPIRY_SECONDS', 604800);
+
+//
+// Include main Wordpress config file.
+//
+$path = sprintf('%s/wp-config.php', dirname(dirname(dirname(dirname(dirname(__FILE__))))));
+if (file_exists($path) == true) {
+	include($path);
+}
+
+//
+// Create fake apply_filters() function in case we can't find the wp-config.php file.
+//
+if (function_exists('apply_filters') == false) {
+	function apply_filters($id, $value) {
+		return $value;
+	}
+}
 
 //
 // Get the requested CSS filename.
@@ -43,22 +60,22 @@ if (strlen($file) > 0) {
 //
 $css = str_replace(
 	array(
+		'[[COLOUR-HIGHLIGHT-PRIMARY]]',
+		'[[COLOUR-HIGHLIGHT-SECONDARY]]',
 		'[[FONT-FAMILY]]',
 		'[[FONT-FAMILY-ESCAPED]]',
 		'[[FONT-WEIGHT-NORMAL]]',
 		'[[FONT-WEIGHT-BOLD]]',
-		'[[FONT-WEIGHT-EXTRA-BOLD]]',
-		'[[COLOUR-HIGHLIGHT-PRIMARY]]',
-		'[[COLOUR-HIGHLIGHT-SECONDARY]]'
+		'[[FONT-WEIGHT-EXTRA-BOLD]]'
 	),
 	array(
-		DERMATOS_FONT_FAMILY,
-		DERMATOS_FONT_FAMILY_ESCAPED,
-		DERMATOS_FONT_WEIGHT_NORMAL,
-		DERMATOS_FONT_WEIGHT_BOLD,
-		DERMATOS_FONT_WEIGHT_EXTRA_BOLD,
-		DERMATOS_COLOUR_HIGHLIGHT_PRIMARY,
-		DERMATOS_COLOUR_HIGHLIGHT_SECONDARY
+		apply_filters('dermatos_colour_highlight_primary_filter', DERMATOS_COLOUR_HIGHLIGHT_PRIMARY),
+		apply_filters('dermatos_colour_highlight_secondary_filter', DERMATOS_COLOUR_HIGHLIGHT_SECONDARY),
+		apply_filters('dermatos_font_family_filter', DERMATOS_FONT_FAMILY),
+		apply_filters('dermatos_font_family_escaped_filter', DERMATOS_FONT_FAMILY_ESCAPED),
+		apply_filters('dermatos_font_weight_normal_filter', DERMATOS_FONT_WEIGHT_NORMAL),
+		apply_filters('dermatos_font_weight_bold_filter', DERMATOS_FONT_WEIGHT_BOLD),
+		apply_filters('dermatos_font_weight_extra_bold_filter', DERMATOS_FONT_WEIGHT_EXTRA_BOLD),
 	),
 	str_replace(array("\x09", "\x0A", "\x0D", ': ', ', ', '{ ', ' {', '; ', ';}', ' }', '} ', ' !'), array(null, null, null, ':', ',', '{', '{', ';', '}', '}', '}', '!'), preg_replace(array('#/\*[^*]*\*+([^/][^*]*\*+)*/#', '#\s[\s]+#'), array(null, ' '), $css))
 );
@@ -68,7 +85,7 @@ $css = str_replace(
 //
 header('Content-Type: text/css; charset: UTF-8');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $modified) . ' GMT');
-header('Expires: ' . gmdate('D, d M Y H:i:s', time() + DERMATOS_CACHE_EXPIRY_SECONDS) . ' GMT');
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + apply_filters('dermatos_cache_expiry_seconds_filter', DERMATOS_CACHE_EXPIRY_SECONDS)) . ' GMT');
 header('Cache-Control: public');
 
 //
